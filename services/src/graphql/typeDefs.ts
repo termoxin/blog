54933187053d7f6e6561c4ecef55b10348aa0da1
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-04-30 20:24:58 +0300

    Feature: create schemas for user, token, response

--- services/src/graphql/typeDefs.ts
+++ services/src/graphql/typeDefs.ts
@@ -1,6 +1,24 @@
 import { gql } from "apollo-server";
 
 const typeDefs = gql`
+	directive @isAuthenticated on FIELD_DEFINITION
+	scalar Date
+
+	type User {
+		id: ID!
+		username: String!
+		password: String!
+	}
+
+	type Token {
+		token: String!
+	}
+
+	type Response {
+		success: Boolean!
+		message: String
+	}
+
 	type Chef {
 		id: ID!
 		name: String!
@@ -15,10 +33,14 @@ const typeDefs = gql`
 	type Mutation {
 		createChef(name: String!): Chef!
 		createRestaurant(chefId: ID!, name: String!): Restaurant!
+		createUser(username: String!, password: String!): User
 	}
 
 	type Query {
 		chefs: [Chef!]!
+		currentUser: User @isAuthenticated
+		users: [User] @isAuthenticated
+		loginUser(username: String!, password: String!): Token
 	}
 `;
 
