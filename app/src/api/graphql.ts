19aec8885a23a2afc1929cf8bd445ebd3ff80e6d
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-05-02 11:50:15 +0300

    Feature: create auth link for apollo

--- app/src/api/graphql.ts
+++ app/src/api/graphql.ts
@@ -1,12 +1,26 @@
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
 export const cache = new InMemoryCache();
 
+const authLink = setContext((_, { headers }: any) => {
+	const token = localStorage.getItem("token");
+
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : ""
+		}
+	};
+});
+
+const httpLink = new HttpLink({ uri: process.env.SERVICES_URI + "/graphql" });
+
 const client = new ApolloClient({
-	cache,
-	link: new HttpLink({ uri: process.env.SERVICES_URI + "/graphql" })
+	link: authLink.concat(httpLink),
+	cache
 });
 
 export default client;
