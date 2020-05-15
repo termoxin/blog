bd78fce4b8fda09684f8462c7577fdbacaef5a44
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-05-15 20:46:25 +0300

    Feature: create tag component

---
+++ app/src/components/Tag.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+	name: string;
+}
+
+export const Tag: React.FC<Props> = ({ name }) => (
+	<span className="cursor-pointer hover:text-white hover:bg-blue-500 inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 ">
+		{name}
+	</span>
+);
