778b132e71222518193b57a7edb287ca9c1bde89
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-05-15 20:45:47 +0300

    Feature: create navigation component

---
+++ app/src/components/Navigation.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Logo } from "./Logo";
+
+export const Navigation = () => (
+	<nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+		<div className="flex items-center flex-shrink-0 text-white mr-6">
+			<Logo />
+			<span className="font-semibold text-xl tracking-tight">Rostislav256</span>
+		</div>
+		<div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+			<div className="text-sm lg:flex-grow">
+				<a
+					href="#"
+					className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-200 mr-4"
+				>
+					Home
+				</a>
+				<a
+					href="#"
+					className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-200 mr-4"
+				>
+					Projects
+				</a>
+				<a href="#" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-gray-200">
+					Blog
+				</a>
+			</div>
+			<div>
+				<a
+					href="#"
+					className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+				>
+					Subscribe
+				</a>
+			</div>
+		</div>
+	</nav>
+);
