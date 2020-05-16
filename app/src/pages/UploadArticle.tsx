74cf03010c902891e830046ea58a8955a8429aa4
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-05-16 22:32:58 +0300

    Feature: add go back button for upload article page

--- app/src/pages/UploadArticle.tsx
+++ app/src/pages/UploadArticle.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { useMutation } from "react-apollo";
 
 import { UPLOAD_ARTICLE } from "../queries";
+import { Button } from "../components/Button";
+import { Link } from "react-router-dom";
 
 export const UploadArticle = () => {
 	const [uploadArticle] = useMutation(UPLOAD_ARTICLE);
@@ -18,6 +20,11 @@ export const UploadArticle = () => {
 		<div className="container p-16">
 			<h1>Upload article</h1>
 			<input required type="file" className="input" onChange={onChange} />
+			<Button className="button ml-2">
+				<Link to="/" className="no-underline">
+					Go back
+				</Link>
+			</Button>
 		</div>
 	);
 };
