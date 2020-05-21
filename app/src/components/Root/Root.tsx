b140b52d32ea47f218c6a9937e38693413ab1243
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-05-21 20:47:35 +0300

    Refactor: get rid of redundant return statement

--- app/src/components/Root/Root.tsx
+++ app/src/components/Root/Root.tsx
@@ -8,27 +8,25 @@ import Profile from "../../pages/Profile";
 import { BlogPost } from "../../pages/BlogPost";
 import { UploadArticle } from "../../pages/UploadArticle";
 
-const Root = () => {
-	return (
-		<Router>
-			<Switch>
-				<PrivateRoute path="/chefs">
-					<Chefs />
-				</PrivateRoute>
-				<Route path="/auth" component={Auth} />
-				<Route path="/article/:id" component={BlogPost} />
-				<PrivateRoute exact path="/">
-					<Profile />
-				</PrivateRoute>
-				<PrivateRoute path="/upload">
-					<UploadArticle />
-				</PrivateRoute>
-				<Route>
-					<h1>Not found</h1>
-				</Route>
-			</Switch>
-		</Router>
-	);
-};
+const Root = () => (
+	<Router>
+		<Switch>
+			<PrivateRoute path="/chefs">
+				<Chefs />
+			</PrivateRoute>
+			<Route path="/auth" component={Auth} />
+			<Route path="/article/:id" component={BlogPost} />
+			<PrivateRoute exact path="/">
+				<Profile />
+			</PrivateRoute>
+			<PrivateRoute path="/upload">
+				<UploadArticle />
+			</PrivateRoute>
+			<Route>
+				<h1>Not found</h1>
+			</Route>
+		</Switch>
+	</Router>
+);
 
 export default Root;
