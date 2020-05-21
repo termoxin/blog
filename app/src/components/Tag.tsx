3ec708b42c888afe0cec510270af9b91e8e5ec3f
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-05-21 20:47:07 +0300

    Feature: add hashtag for Tag component

--- app/src/components/Tag.tsx
+++ app/src/components/Tag.tsx
@@ -6,6 +6,6 @@ interface Props {
 
 export const Tag: React.FC<Props> = ({ name }) => (
 	<span className="cursor-pointer hover:text-white hover:bg-blue-500 inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 ">
-		{name}
+		#{name}
 	</span>
 );
