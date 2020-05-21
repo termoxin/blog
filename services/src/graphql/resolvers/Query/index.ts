e2eed6726fae837fce4ecebd226c6e5228181db3
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-05-21 20:52:16 +0300

    Feature: export article query

--- services/src/graphql/resolvers/Query/index.ts
+++ services/src/graphql/resolvers/Query/index.ts
@@ -2,3 +2,4 @@ export { default as chefs } from "./chefs";
 export { default as currentUser } from "./currentUser";
 export { default as loginUser } from "./loginUser";
 export { default as articles } from "./articles";
+export { default as article } from "./article";
