0bdf5dcfbed05c4538704ae5e8e07ea54a546346
Rostislav Futornoy <rostislav.futornoy@gmail.com>
2020-05-21 20:52:00 +0300

    Feature: export resolvers

--- services/src/graphql/resolvers/Mutation/index.ts
+++ services/src/graphql/resolvers/Mutation/index.ts
@@ -1,3 +1,6 @@
 export { default as createChef } from "./createChef";
 export { default as createRestaurant } from "./createRestaurant";
 export { default as createUser } from "./createUser";
+export { default as createArticle } from "./createArticle";
+export { default as uploadArticle } from "./uploadArticle";
+export { default as attachTag } from "./attachTag";
